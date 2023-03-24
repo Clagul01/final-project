3c2806c3880ad12a68a0016d3e710b1c06219682
unknown <ateemichael@yahoo.com>
2023-03-24 12:44:10 +0000

    modified home box for visibility

--- src/views/home.js
+++ src/views/home.js
@@ -16,6 +16,7 @@ return(
   <figure class="image column is-4" >
   <img class="" src={MainLogo} alt="{index.bev}" style={{width: 350 + "px", height: 350 + "px", top: 20 + "px"}}/>
   </figure>
+  <div class="colored-box">
     <p class="title column is-9 is-family-monospace">
       Kaf-Fet (<i>Kah-Feht</i>):
       <hr/>
@@ -27,7 +28,7 @@ return(
     <span><button onClick={navigateToOrder}class="button is-danger">Order Now</button></span>    <span><button class="button is-primary"><a href="https://github.com/Clagul01/final-project">Discover More About Us</a></button></span>
 
     </p>
-   
+    </div>
   </div>
 </section>
 );
